4bcda9a810edc296e3c3b494160c84a3aced658e
handar <hadiaandar@gmail.com>
2021-02-09 17:14:58 -0800

    set up return value for movieAtIndex function in MovieManager.swift

--- FilmFestTutorial/Models/MovieManager.swift
+++ FilmFestTutorial/Models/MovieManager.swift
@@ -11,12 +11,11 @@ class MovieManager {
     var moviesToSeeCount = 0 //let is constant variable, so change the type to var, changing this shows how fluid test driven development can be. Don't overthink the applicatin logic too far ahead of what you need in the present. Your code will be cleaner and less error prone.
     let moviesSeenCount = 0
     
-    func addMovie(movie: Movie){ //function to take in a movie of type movie
+    func addMovie(movie: Movie){ //function to take in a movie of type movie to add it to your list
         moviesToSeeCount += 1 //increment the count to 1
     }
     
     func movieAtIndex(index: Int) -> Movie { //the function will take in an index and return a Movie which will have a title and optional release date from Movie.swift
-        
-        
+        return Movie(title: "Teen Comedy") //return a default movie
     }
 }
